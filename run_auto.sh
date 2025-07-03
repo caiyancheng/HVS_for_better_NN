c4f378ffdf0eef4d595439f7952cb7622081df1a
caiyancheng <19307140030@fudan.edu.cn>
2025-07-03 20:40:55 +0100

    2025-7-3

--- run_auto.sh
+++ run_auto.sh
@@ -3,7 +3,7 @@
 # 激活conda环境（假设环境名为myenv）
 # 注意：激活conda环境要用source并指定conda.sh路径
 source ~/anaconda3/etc/profile.d/conda.sh
-conda activate myenv
+conda activate hvs
 cd /auto/homes/yc613/Py_codes/HVS_for_better_NN/
 
 python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF_masking_transducer.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 5
