7257cff47552720017b1eb7df837ed00b224bd67
caiyancheng <19307140030@fudan.edu.cn>
2025-07-04 15:59:46 +0100

    2025-7-3

--- run_auto.sh
+++ run_auto.sh
@@ -6,28 +6,28 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate hvs
 cd /auto/homes/yc613/Py_codes/HVS_for_better_NN/
 
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 5 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 10 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 20 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 30 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 40 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 50 #
-
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 5 --peak_luminance 500 --diagonal_size_inches 5 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 5 --peak_luminance 500 --diagonal_size_inches 10 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 5 --peak_luminance 500 --diagonal_size_inches 20 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 5 --peak_luminance 500 --diagonal_size_inches 30 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 5 --peak_luminance 500 --diagonal_size_inches 40 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 5 --peak_luminance 500 --diagonal_size_inches 50 #
-
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 100 --diagonal_size_inches 5 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 100 --diagonal_size_inches 10 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 100 --diagonal_size_inches 20 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 100 --diagonal_size_inches 30 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 100 --diagonal_size_inches 40 #
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 100 --diagonal_size_inches 50 #
-
-python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF_2.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 5 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 5 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 10 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 20 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 30 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 40 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 50 #
+#
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 5 --peak_luminance 500 --diagonal_size_inches 5 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 5 --peak_luminance 500 --diagonal_size_inches 10 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 5 --peak_luminance 500 --diagonal_size_inches 20 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 5 --peak_luminance 500 --diagonal_size_inches 30 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 5 --peak_luminance 500 --diagonal_size_inches 40 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 5 --peak_luminance 500 --diagonal_size_inches 50 #
+#
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 100 --diagonal_size_inches 5 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 100 --diagonal_size_inches 10 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 100 --diagonal_size_inches 20 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 100 --diagonal_size_inches 30 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 100 --diagonal_size_inches 40 #
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF.py --pyr_levels 4 --peak_luminance 100 --diagonal_size_inches 50 #
+#
+#python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF_2.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 5 #
 python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF_2.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 10 #
 python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF_2.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 20 #
 python Train_Laplacian_NN/train_resnet_cifar100_no_first_downsample_dkl_contrast_lpyr_CSF_2.py --pyr_levels 4 --peak_luminance 500 --diagonal_size_inches 30 #
