77dfca3c8b1ead1c7d50cb7b09ca708b1568cb01
caiyancheng <19307140030@fudan.edu.cn>
2025-07-12 13:36:11 +0100

    2025-7-12

--- train_main.py
+++ train_main.py
@@ -1,4 +1,5 @@
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 import numpy as np
 import torch.optim as optim
 import torch.nn as nn
@@ -101,13 +102,14 @@ def train_model(model, trainloader, testloader, optimizer, scheduler, criterion,
 if __name__ == '__main__':
     train_dataset_name_list = ['Tiny-ImageNet'] #'CIFAR-100']#,
     # model_name_list = ['resnet18', 'resnet18-lpyr', 'resnet18-clpyr', 'resnet18-clpyr-CSF', 'resnet18-clpyr-CM-transducer']
-    model_name_list = ['resnet18', 'resnet18-lpyr', 'resnet18-lpyr-2', 'resnet18-clpyr', 'resnet18-clpyr-CSF', 'resnet18-clpyr-CM-transducer']
+    # model_name_list = ['resnet18', 'resnet18-lpyr', 'resnet18-lpyr-2', 'resnet18-clpyr', 'resnet18-clpyr-CSF', 'resnet18-clpyr-CM-transducer']
+    model_name_list = ['resnet18-lpyr', 'resnet18-clpyr', 'resnet18-clpyr-CSF', 'resnet18-clpyr-CM-transducer']
     color_space_name_list = ['sRGB', 'RGB_linear', 'XYZ_linear', 'DKL_linear']
-    peak_luminance_list = [100, 200, 500]
-    diagonal_size_inches_list = [10, 20, 50] #5
+    peak_luminance_list = [100]#, 200, 500]
+    diagonal_size_inches_list = [10]#, 20, 50] #5
     resolution = [64, 64]
     viewing_distance_meters = 1
-    batch_size = 128 * 8
+    batch_size = 128 * 8 #* 2
 
     for dataset_name in train_dataset_name_list:
         for model_name in model_name_list:
